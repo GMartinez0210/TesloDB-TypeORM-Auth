2893487afa2e721271d0401d231aef512393149a
GMartinez0210 <genaro010204@gmail.com>
2023-03-17 00:15:39 -0500

    Missing coding the strategy

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -5,9 +5,25 @@ import { User } from './entities/user.entity';
 
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [
+    TypeOrmModule.forFeature([User]),
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        secret: configService.get('JWT_SECRET'),
+        signOptions: {
+          expiresIn: '7d',
+        },
+      }),
+    }),
+  ],
   controllers: [AuthController],
   providers: [AuthService],
   exports: [TypeOrmModule, AuthService],
